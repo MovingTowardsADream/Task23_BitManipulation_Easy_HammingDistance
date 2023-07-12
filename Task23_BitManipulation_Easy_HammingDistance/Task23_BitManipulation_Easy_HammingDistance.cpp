c05ef2536ceebc536f64f852f82eb4624df3db82
Matvey <zxdfcvas4554@mail.ru>
2023-07-12 18:17:44 +0300

    New version

--- Task23_BitManipulation_Easy_HammingDistance/Task23_BitManipulation_Easy_HammingDistance.cpp
+++ Task23_BitManipulation_Easy_HammingDistance/Task23_BitManipulation_Easy_HammingDistance.cpp
@@ -3,11 +3,26 @@
 class Solution {
 public:
     static int hammingDistance(int x, int y) {
-        return 0;
+        // 1
+        if (y > x)
+            std::swap(x, y);
+        int tmp1, tmp2, count = 0;
+        while (x != 0) {
+            tmp1 = x & 1;
+            tmp2 = y & 1;
+            if (tmp1 != tmp2) {
+                count++;
+            }
+            x >>= 1;
+            y >>= 1;
+        }
+        return count;
+        // 2
+        // return __builtin_popcount(x | y) - __builtin_popcount(x & y);
     }
 };
 
 int main()
 {
-    std::cout << Solution::hammingDistance(1, 4);
+    std::cout << Solution::hammingDistance(3, 1);
 }
