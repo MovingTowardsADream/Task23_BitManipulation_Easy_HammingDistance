1125dcb081c5273f222cb42ffd7f8215573aacfa
Matvey <zxdfcvas4554@mail.ru>
2023-07-12 18:01:41 +0300

    Add project files.

---
+++ Task23_BitManipulation_Easy_HammingDistance/Task23_BitManipulation_Easy_HammingDistance.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+class Solution {
+public:
+    static int hammingDistance(int x, int y) {
+        return 0;
+    }
+};
+
+int main()
+{
+    std::cout << Solution::hammingDistance(1, 4);
+}
